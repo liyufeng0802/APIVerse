8418d0074d99facb8ae192628e19527a55f7a434
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2023-10-29 01:19:36 -0700

    Update import_modoule.py

--- import_module/import_module.py
+++ import_module/import_module.py
@@ -2,6 +2,7 @@ from gpt_module import GPT_module
 import requests
 import re
 import json
+from bs4 import BeautifulSoup
 
 
 class Import_module:
@@ -13,7 +14,8 @@ class Import_module:
     def addNewURL(self, url):
         html_format = requests.get(url)
         text = html_format.text
-        prompt = f"On this website (at the bottom of this prompt), is there an API documentation?\nIf there is no API documentation on the webpage, print me a No. If there is API documentation, can you write me the curl requests with those APIs that do not require authentication including descriptions?  I want you summarize the available APIs in an python array list format, where each list element is a tuple, ex [(curl request1, description for the endpoint1), (curl request2, description for the endpoint2)]. If using any of the quotes for the tuples, only use single quote. also, avoid any other special chars, such as $, #, etc, just give me clean tuple lists. you have to use single quoteBelow is the content of the website: {text}"
+        text = clean_html(text)
+        prompt = f"On this website (at the bottom of this prompt), is there an API documentation?\nIf there is no API documentation on the webpage, print me a No. If there is API documentation, can you write me the curl requests with those APIs that do not require authentication including descriptions?  I want you summarize the available APIs in an python array list format, where each list element is a tuple, ex [(curl request1, description for the endpoint1), (curl request2, description for the endpoint2)]. If using any of the quotes for the tuples, only use single quote. also, avoid any other special chars, such as $, #, etc, just give me clean tuple lists. When include the text content, don't include the comma of thar text, since it gonna be conflit with the comma of my list items. you have to use single quoteBelow is the content of the website: {text}"
         # print(prompt)
         test_model = GPT_module()
         response = test_model.query(prompt)
@@ -31,6 +33,7 @@ class Import_module:
         print(curl_requests)
         regex = r"\(.*\)"
         curl_list = re.findall(regex, curl_requests)
+        print("the list: ", curl_list)
         self.saveCurlList(curl_list)
     
     def saveCurlList(self, curl_list):
@@ -59,4 +62,18 @@ class Import_module:
     
     def get_jsonResponse(self):
         return self.data
-        
+
+
+def clean_html(html_text):
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_text, "html.parser")
+
+    # Remove script and style elements
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()  # Remove the tag and its content
+
+    # Get the text content of the HTML
+    text = soup.get_text(separator='\n', strip=True)
+
+    # Return the cleaned text
+    return text
